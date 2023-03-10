2433e957859a650314eb52cdaa4eab5aaeeef0d3
typeperfest <perfect2111@gmail.com>
2023-03-10 14:08:37 +0300

    initial

---
+++ ClusterAnalysisOfCollages/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClusterAnalysisOfCollages
+{
+    class Program
+    {
+        public static double calculataDistance(double x1, double y1, double x2, double y2)
+            => Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        public record Hospital (string name, (double x, double y) location, string district);
+        public record District (string name, (double x, double y) location);
+
+        static void Main(string[] args)
+        {
+            JArray hospitalsRaw = JArray.Parse(File.ReadAllText("Hospitals.json"));
+            JObject districtsRaw = JObject.Parse(File.ReadAllText("DistrictsLocations.json"));
+
+            IEnumerable<District> distritsQuery =
+                from d in districtsRaw["Districts"]
+                select new District((string)d["FullName"],
+                                    ((double)d["Coordinates"][0], (double)d["Coordinates"][1]));
+
+            IEnumerable<Hospital> hospitalsQuery =
+                from h in hospitalsRaw
+                let cords = h["geoData"]["coordinates"][0]
+                select new Hospital((string)h["ShortName"],
+                         ((double)cords[1], (double)cords[0]),
+                         (string)h["ObjectAddress"][0]["AdmArea"]);
+
+            uint correntPredictions = 0;
+            foreach (var hospital in hospitalsQuery)
+            {
+                (string _, var (hx, hy), string bindedDistrictName) = hospital;
+                var predictedDistrictName = distritsQuery
+                    .GroupBy(
+                        distr => calculataDistance(distr.location.x, distr.location.y, hx, hy),
+                        distr => distr.name
+                    )
+                    .OrderBy(el => el.Key)
+                    .First()
+                    .First();
+                Console.WriteLine("{0} is predicted district", predictedDistrictName);
+                if (predictedDistrictName == bindedDistrictName)
+                {
+                    ++correntPredictions;
+                }
+            }
+            double accuracy = (double)correntPredictions / hospitalsQuery.Count();
+            Console.WriteLine("Accuracy is: {0}%", accuracy * 100);
+        }
+    }
+}
---
+++ ClusterAnalysisOfCollages/TrainData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ClusterAnalysisOfCollages
+{
+    public static class DistrictsDictionaty
+    {
+        private static Dictionary<string, List<bool>> namesCodes;
+
+        public static void readFromFile(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            var line = sr.ReadLine();
+            while (line != null)
+            {
+                string districtName = line.Split(':')[0];
+                string districtCodeString = line.Split(':')[1];
+                List<bool> districtCodeBool = districtCodeString
+                    .Substring(1, districtCodeString.Count() - 2) // throw bracets away
+                    .Split(',')
+                    .Cast<bool>()
+                    .ToList();
+                namesCodes.Add(districtName, districtCodeBool);
+                line = sr.ReadLine();
+            }
+        }
+
+        public static List<bool> getCode(string name)
+        {
+            return namesCodes[name];
+        }
+    }
+
+    public record TrainData(List<Collage> Collages, List<District> Districts);
+
+    public record Collage(Location Location, string Name);
+
+    public record District(Location Location, List<bool> Code);
+
+    public class Location
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
